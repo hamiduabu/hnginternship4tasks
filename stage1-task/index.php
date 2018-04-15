507b41cb5a93f06f32d5e0d8f177112dbd79594e
Hamidu Abu <hamiduabu@live.com>
2018-04-15 17:22:43 +0100

    added figma-design-link and a few modifications

--- stage1-task/index.php
+++ stage1-task/index.php
@@ -1,43 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="https://fonts.googleapis.com/css?family=Pacifico|Poppins|Rubik"
     rel="stylesheet">
-
   <link rel="stylesheet" href="css/main.css">
   <title>Stage 1 Task</title>
 </head>
-
 <body>
   <div>
-    <center>
-      <div class="treetop">
-        <p>In time,<br>
+    <div class="treetop">
+      <p>In time,
+        <br>
         <q>from a small seed,
           <br> a mighty trunk may grow
         </q>
         <br>- Aeschylus.</p>
-      </div>
-      <div class="treetrunk">
-        <p>|||||</p>
-        <p>|||||</p>
-        <p>|||||</p>
-        <div class="line"></div>
-
-      </div>
-      <div class="datetime">			
+    </div>
+    <div class="treetrunk">
+      <p>|||||</p>
+      <p>|||||</p>
+      <p>|||||</p>
+      <div class="line"></div>
+    </div>
+    <div class="datetime">
         <p><?php date_default_timezone_set("Africa/Lagos"); echo  date("g : i : s A");?></p>
         <p><?php echo date('d - M - y')?></p>
               </div>
 			<div class="comment"><p>What would you like to do today?</p><div>
-    </center>
-  </div>
-
-
+    </div>
 </body>
-
 </html>
